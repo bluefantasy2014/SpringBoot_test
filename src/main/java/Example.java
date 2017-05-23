8cb203c28dfea82f49e975173abd2d8bd343a0a2
admin <admin@glzc>
2017-05-23 11:05:12 +0800

    test

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,4 +18,23 @@ public class Example {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Example.class, args);
 	}
+	
+	@Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		Exception e =  new Exception(); 
+		System.out.println("print call stack --------"); 
+		e.printStackTrace(System.out);
+		System.out.println("print call stack end --------"); 
+		
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+            System.out.println("测试打印当前的bean");
+            System.out.println(this);
+        };
+    }
 }
